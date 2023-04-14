ba8bd935cbfb93577e1c21e667287aed82913dde
mikha <Mikhailov.dzenphone@gmail.com>
2023-04-14 15:18:52 +0300

    Task 2

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         task1();
+        task2();
     }
 
     public static int[] generateRandomArray() {
@@ -22,5 +23,23 @@ public class Main {
             totalCost += bookCost[i];
         }
         System.out.println("Сумма трат за месяц составила " + totalCost + " рублей.");
+    }
+
+    public static void task2() {
+        int[] bookCost = generateRandomArray();
+        System.out.println();
+        System.out.println("Задача 2");
+        System.out.println(Arrays.toString(bookCost));
+        int maxCost = -1;
+        int minCost = 200_001;
+        for (int i : bookCost) {
+            if (i > maxCost) {
+                maxCost = i;
+            } else if (i < minCost) {
+                minCost = i;
+            }
+        }
+        System.out.println("Минимальная сумма трат составила " + minCost + " рублей.");
+        System.out.println("Максимальная сумма трат составила " + maxCost + " рублей.");
     }
 }
